471ca3842f97f587e0698eccf5d3589dcf356e75
Oskar Bator <os.bator@wp.pl>
2020-03-08 09:22:28 +0100

    Szkielet projektu

---
+++ src/main/java/Product.java
@@ -0,0 +1,43 @@
+import java.security.PrivateKey;
+
+public class Product {
+
+
+        private String name;
+        private int price;
+        private String category;
+
+
+    public Product(String name, int price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Product() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
