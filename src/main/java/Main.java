d1767447a7636113f0d2bd8095da0c766b4332d9
Oskar Bator <os.bator@wp.pl>
2020-03-08 10:18:27 +0100

    Szkielet projektu 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,25 @@
 public class Main {
 
 
+    public static void main(String[] args) {
+
+
+        Product product = new Product("Kasza", 15, "Jedzenie");
+        Product product1 = new Product("Proszek", 20, "Czystosc");
+        Product product2 = new Product("Woda", 3, "Picie");
+        Product product3 = new Product("Masło", 5, "Jedzenie");
+
+        Customer customer = new Customer("Jan", "Kowalski", 20, "jan.kowalski@wp.pl");
+        Customer customer1 = new Customer("Anna", "Banan", 12, "anna.banan@wp.pl");
+        Customer customer2 = new Customer("Jacek", "Kolos", 28, "jacek.kolos@wp.pl");
+        Customer customer3 = new Customer("Marek", "Marek", 20, "marek.marek@wp.pl");
+
+        Order order = new Order(10,14);
+        Order order1 = new Order(2,12);
+        Order order2 = new Order(1,11);
+
+
+        System.out.println(customer1.);
+
+    }
 }
--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,4 +1,40 @@
-public class Order {
+public class Order  {
 
-    
+    private String product ;
+    private int quantity ;
+    private int orderDate ;
+    private Customer customer;
+    private Product Product;
+
+
+
+
+
+    public Order( int quantity, int orderDate) {
+        this.product = product;
+        this.quantity = quantity;
+        this.orderDate = orderDate;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getOrderDate() {
+        return orderDate;
+    }
+
+    public Order(Product Product)
+    {
+        this.Product = Product;
+    }
+
+
+    public void setOrderDate(int orderDate) {
+        this.orderDate = orderDate;
+    }
 }
